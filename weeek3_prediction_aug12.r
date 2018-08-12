e0babfcdb561218c56541444ad7f4c7815d648cb
Rui Hu <maggiehu.china@gmail.com>
2018-08-12 14:05:27 -0400

    created search bigram function

--- weeek3_prediction_aug12.r
+++ weeek3_prediction_aug12.r
@@ -4,13 +4,6 @@ library(DT)
 library(sqldf)
 library(ngram)
 library(stringr)
-#### Function to create data tables frome each data table list
-create_dt<-function(data_list){
-  dt_uni<-data_list[[1]]
-  dt_bi<-data_list[[2]]
-  dt_tri<-data_list[[3]]
-  dt_qua<-data_list[[4]]
-}
 
 #### Function to create ngram tables, including the ngram, the frequency, and each word in the ngram, for each ngram.
 create_ngram_table<-function(ngram,n){
@@ -43,27 +36,56 @@ blog_bi_table<-create_ngram_table(data_blog,2)
 blog_uni_table<-create_ngram_table(data_blog,1)
 
 #### Type in a phrase
-typed<-"of the"
+typed<-"of"
 n<-wordcount(typed)
 typed_word<-vector(mode="character",length=n)
 for (i in 1:n)
   typed_word[i]<-word(typed,i)
 
+#### Function to search in bigram 
+search_bigram<-function(bi_table,uni_table,input){
+  matching_dt<-bi_table[V1==input[1]] 
+  if(nrow(matching_dt)){
+    sorted_dt<-matching_dt[order(-frequency)]
+    predicted_word<-sorted_dt[1,V1]
+  }else 
+    sorted_dt<-uni_table[order(-frequency)]
+    predicted_word<-sorted_dt[1,V2]
+  return(predicted_word)
+}
+
+if (n==1){
+  predicted<-search_bigram(blog_bi_table,blog_uni_table,typed_word)
+  print(predicted)
+}
+
 ## If n==1 then search bi for matching uni word
 if (n==1) {
-  matching_dt<-blog_bi_table[grep(paste("^",typed_word[1],"$",sep=""),blog_bi_table$V1)] 
-  sorted_dt<-matching_dt[order(-frequency)]
-  predicted_word<-sorted_dt[1,V2]
+  matching_dt<-blog_bi_table[V1==typed_word[1]] 
+  if(nrow(matching_dt)){
+    sorted_dt<-matching_dt[order(-frequency)]
+    predicted_word<-sorted_dt[1,V1]
+  }else 
+    sorted_dt<-blog_uni_table[order(-frequency)]
+    predicted_word<-sorted_dt[1,V2]
 }
 
 #if (n=2) then search tri for matching bi words
 if (n==2){
-  ##matching_dt<-blog_tri_table[(grep(paste("^",typed_word[1],"$",sep=""),blog_tri_table$V1))&&grep(paste("^",typed_word[2],"$",sep=""),blog_tri_table$V2)]
   matching_dt<-blog_tri_table[(V1==typed_word[1])&(V2==typed_word[2])]
-  sorted_dt<-matching_dt[order(-frequency)]
-  predicted_word<-sorted_dt[1,V3]
+  if(nrow(matching_dt)!=0){
+    sorted_dt<-matching_dt[order(-frequency)]
+    predicted_word<-sorted_dt[1,V3]
+  }else ## search bi fro matching uni word
+  
 }
+
 #if (n=3) then search qua for matching three words
+if (n==3){
+  matching_dt<-blog_qua_table[(V1==typed_word[1])&(V2==typed_word[2])&(V3==typed_word[3])]
+  sorted_dt<-matching_dt[order(-frequency)]
+  predicted_word<-sorted_dt[1,V4]
+}
 #if (n>3) then only take the last 3 words of typed
 
 
